fc8c2e947084ad3a230ac1235c44d68af25d317c
Taga Yasunori <yasukun.taga@gmail.com>
2020-08-27 02:09:04 +0900

    5-2

---
+++ 5/enum-sample/src/main.rs
@@ -0,0 +1,37 @@
+fn main() {
+    let some_u8_value = Some(0u8);
+    match some_u8_value {
+        Some(3) => println!("three"),
+        _ => (),
+    }
+
+    // 上記と同じ
+    if let Some(3) = some_u8_value {
+        println!("three");
+    }
+}
+
+#[derive(Debug)]
+enum UsState {
+    Alabama,
+    Alaska,
+}
+
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter(UsState),
+}
+
+fn value_in_cents(coin: Coin) -> u32 {
+    match coin {
+        Coin::Penny => 1,
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter(state) => {
+            println!("State quarter from {:?}!", state);
+            25
+        }
+    }
+}
