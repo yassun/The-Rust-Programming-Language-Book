2a3401dcaa7273937990d2dfb88e2a7571eff85e
Taga Yasunori <yasukun.taga@gmail.com>
2020-08-27 00:27:38 +0900

    5-2 tuple

--- 4/rectangles/src/main.rs
+++ 4/rectangles/src/main.rs
@@ -1,4 +1,17 @@
 fn main() {
+    let rect1 = (30, 50);
+
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        area(rect1)
+    );
+}
+
+fn area(dimensions: (u32, u32)) -> u32 {
+    dimensions.0 * dimensions.1
+}
+/*
+fn main() {
     let width1 = 30;
     let height1 = 50;
 
@@ -11,4 +24,4 @@ fn main() {
 
 fn area(width: u32, height: u32) -> u32 {
     width * height
-}
+}*/
