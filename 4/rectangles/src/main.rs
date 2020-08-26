729b60bbccc2ef00ec17563f3c982a40db78e2d7
Taga Yasunori <yasukun.taga@gmail.com>
2020-08-27 00:32:24 +0900

    5-2 implementation

--- 4/rectangles/src/main.rs
+++ 4/rectangles/src/main.rs
@@ -1,3 +1,28 @@
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+}
+
+fn main() {
+    let rect1 = Rectangle {
+        width: 30,
+        height: 50,
+    };
+
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        rect1.area()
+    );
+}
+
+/*j
 struct Rectangle {
     width: u32,
     height: u32,
@@ -18,7 +43,6 @@ fn main() {
 fn area(rectangle: &Rectangle) -> u32 {
     rectangle.width * rectangle.height
 }
-/*
 fn main() {
     let rect1 = (30, 50);
 
