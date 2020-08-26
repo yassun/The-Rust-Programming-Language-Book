a4a5054856be71d006b40aeb66858d1b010be1ef
Taga Yasunori <yasukun.taga@gmail.com>
2020-08-27 00:42:01 +0900

    5-2 Rectangle

--- 4/rectangles/src/main.rs
+++ 4/rectangles/src/main.rs
@@ -8,6 +8,18 @@ impl Rectangle {
     fn area(&self) -> u32 {
         self.width * self.height
     }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+
+    // 関連関数: let sq = Rectangle::square(3)
+    fn square(size: u32) -> Rectangle {
+        Rectangle {
+            width: size,
+            height: size,
+        }
+    }
 }
 
 fn main() {
