0bb5578edd21e5317b000c638d1e6c16459233a5
Taga Yasunori <yasukun.taga@gmail.com>
2020-08-27 00:28:38 +0900

    5-2 struct

--- 4/rectangles/src/main.rs
+++ 4/rectangles/src/main.rs
@@ -1,3 +1,24 @@
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+fn main() {
+    let rect1 = Rectangle {
+        width: 30,
+        height: 50,
+    };
+
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        area(&rect1)
+    );
+}
+
+fn area(rectangle: &Rectangle) -> u32 {
+    rectangle.width * rectangle.height
+}
+/*
 fn main() {
     let rect1 = (30, 50);
 
@@ -10,7 +31,7 @@ fn main() {
 fn area(dimensions: (u32, u32)) -> u32 {
     dimensions.0 * dimensions.1
 }
-/*
+
 fn main() {
     let width1 = 30;
     let height1 = 50;
